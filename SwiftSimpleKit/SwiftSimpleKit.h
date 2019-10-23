a2f9b96f7a9d41ed77d05d3bacccabb8e8f6865d
Jonah Pelfrey <pelfreyjonah@gmail.com>
2019-10-23 15:39:19 -0500

    initial setup

---
+++ SwiftSimpleKit/SwiftSimpleKit.h
@@ -0,0 +1,19 @@
+//
+//  SwiftSimpleKit.h
+//  SwiftSimpleKit
+//
+//  Created by Jonah Pelfrey on 10/23/19.
+//  Copyright © 2019 Jonah Pelfrey. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftSimpleKit.
+FOUNDATION_EXPORT double SwiftSimpleKitVersionNumber;
+
+//! Project version string for SwiftSimpleKit.
+FOUNDATION_EXPORT const unsigned char SwiftSimpleKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftSimpleKit/PublicHeader.h>
+
+
---
+++ SwiftSimpleKitExamples/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftSimpleKitExamples
+//
+//  Created by Jonah Pelfrey on 10/23/19.
+//  Copyright © 2019 Jonah Pelfrey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
